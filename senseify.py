d8f98b0255d56df2ffcb88c80e1bbbc7001b96c4
Gellatio <26261205+gellatio@users.noreply.github.com>
2021-05-30 16:35:17 -0400

    Added IndexError exception if album art not found
    This error typically pops up on album art-less songs or local files, since Spotify cannot obtain art for local files.

--- senseify.py
+++ senseify.py
@@ -29,16 +29,29 @@ while True:
 	fullinfo = tname + " - " + tartist
 
 	if SHOW_COVER:
-		tcover = track['item']['album']['images'][0]['url']
-		urllib.request.urlretrieve(tcover, 'cover.png')
-		image = Image.open('cover.png')
-		new_image = image.resize((8, 8))
-		new_image.save('cover_8.png')
-		an_image = Image.open("cover_8.png")
-		sequence_of_pixels = an_image.getdata()
-		list_of_pixels = list(sequence_of_pixels)
+		try:
+			tcover = track['item']['album']['images'][0]['url']
+			urllib.request.urlretrieve(tcover, 'cover.png')
+			image = Image.open('cover.png')
+			new_image = image.resize((8, 8))
+			new_image.save('cover_8.png')
+			an_image = Image.open("cover_8.png")
+			sequence_of_pixels = an_image.getdata()
+			list_of_pixels = list(sequence_of_pixels)
+			#print(list_of_pixels)
+			#print(str(len(list_of_pixels)))
+			#print(tcover)
+			coverAvail = 1
+		except IndexError as e:
+			print("ERROR!")
+			print(e)
+			print("Do not panic, this is normal is you are playing from local files.")
+			coverAvail = 0
 
 	sense.show_message(fullinfo)
 	if SHOW_COVER:
-		sense.set_pixels(list_of_pixels)
+		if coverAvail == 1:
+			sense.set_pixels(list_of_pixels)
+		if coverAvail == 0:
+			print("Did not show cover since cover not available")
 	time.sleep(5)
